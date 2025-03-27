4b96750d7cb0d22bc69eaeaa093ac1801d0fe119
LongLTCB <bquangnam2021@gmail.com>
2025-03-27 16:47:18 +0700

    first commit

---
+++ Assets/InventoryManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using static UnityEditor.Progress;
+
+public class InventoryManager : MonoBehaviour
+{
+    public ItemSlot[] itemSlots; // Mảng các ô chứa item
+
+    public void AddItem(Item newItem)
+    {
+        foreach (ItemSlot slot in itemSlots)
+        {
+            if (slot != null)
+            {
+                slot.SetItem(newItem);
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/ItemSlot.cs
@@ -0,0 +1,27 @@
+﻿using static UnityEditor.Progress;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    public Image ItemIcon;  // Biến chứa UI hình ảnh item
+    private Item item;      // Biến chứa thông tin item
+
+    // Gán item vào slot
+    public void SetItem(Item newItem)
+    {
+        item = newItem; // Lưu item vào slot
+
+        if (item != null)
+        {
+            ItemIcon.sprite = item.icon; // Gán hình ảnh của item vào UI
+            ItemIcon.enabled = true;
+        }
+        else
+        {
+            ItemIcon.sprite = null;
+            ItemIcon.enabled = false;
+        }
+    }
+}
+
